c284e7d3e4ca30fd6cf81dddb8b49ea8f1f58056
Timur Radko <timurradko@gmail.com>
2020-02-13 11:36:40 +0300

    Search for duplicate Strings in an Array and write them to HashMap

--- RepeatingStrings2.java
+++ RepeatingStrings2.java
@@ -3,25 +3,26 @@ package com.timurradko.repeatingStrings;
 import java.util.HashMap;
 import java.util.Map;
 
-public class repeatingStrings {
+public class RepeatingStrings2 {
     public static void main(String[] args) {
         String[] array = {"W", "W", "L", "L", "P", "J", "U", "Y", "R", "E", "P", "Y"};
         Map<String, Integer> collection = new HashMap<>();
         int count = 0;
         for (int i = 0; i < array.length; i++) {
-            String arrayString = array[i];
-            for (int j = 0; j < array.length - 1; j++) {
-                if (arrayString.equals(array[j])) {
-                    count++;
-                }
-            }
             collection.put(array[i], count);
-            count = 0;
+            if (collection.containsKey(array[i])) {
+                count++;
+            }
+
+            //count = 0;
         }
+
         for (Map.Entry<String, Integer> pair : collection.entrySet()) {
-            String key = pair.getKey();
-            Integer value = pair.getValue();
-            System.out.println(key + " " + value);
+            String s = pair.getKey();
+            int i = pair.getValue();
+            System.out.println(s + " : " + i);
         }
     }
+
+
 }
